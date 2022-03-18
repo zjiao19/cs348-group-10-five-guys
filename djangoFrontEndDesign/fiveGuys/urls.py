05534fa374651b3640789980e3ac115a16b8effa
LowenSunday <luwh2000@gmail.com>
2022-03-18 13:51:26 -0400

    More UI pages

--- djangoFrontEndDesign/fiveGuys/urls.py
+++ djangoFrontEndDesign/fiveGuys/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('menu', views.menu, name='menu'),
     path('cart', views.cart, name='cart'),
+    path('orderHistory', views.orderHistory, name='orderHistory'),
+    path('message', views.message, name='message'),
+    path('stockManagement', views.stockManagement, name='stockManagement'),
 ]
--- djangoFrontEndDesign/fiveGuys/views.py
+++ djangoFrontEndDesign/fiveGuys/views.py
@@ -6,9 +6,17 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse(render(request, 'index.html', context={}))
 
-
 def menu(request):
     return HttpResponse(render(request, 'menu.html', context={}))
 
 def cart(request):
     return HttpResponse(render(request, 'cart.html', context={}))
+
+def orderHistory(request):
+    return HttpResponse(render(request, 'orderHistory.html', context={}))
+
+def message(request):
+    return HttpResponse(render(request, 'message.html', context={}))
+
+def stockManagement(request):
+    return HttpResponse(render(request, 'stockManagement.html', context={}))
