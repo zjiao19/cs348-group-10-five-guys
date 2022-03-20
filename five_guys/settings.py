1c80951382f297d2b7247fb27bab49eef1938136
zjiao19 <zjiao19@outlook.com>
2022-03-20 14:19:16 -0400

    add static path

--- five_guys/settings.py
+++ five_guys/settings.py
@@ -125,3 +125,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = ['static']
